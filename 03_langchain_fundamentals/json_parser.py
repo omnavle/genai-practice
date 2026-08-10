44a664af4cceaec67b52556b33bb2c6fef0d0549
Om Navale <omnavale899@gmail.com>
2026-08-10 10:08:32 +0530

    Parser Created

---
+++ 03_langchain_fundamentals/json_parser.py
@@ -0,0 +1,32 @@
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+
+llm = ChatGroq(
+    model="llama-3.1-8b-instant",
+    temperature=0
+)
+
+prompt = PromptTemplate(
+    template="""
+    Give information about the following person.
+
+    Return the answer in JSON format with these keys:
+    name
+    age
+    profession
+
+    Person: {person}
+    """,
+    input_variables=["person"]
+)
+
+parser = JsonOutputParser()
+
+chain = prompt | llm | parser
+
+response = chain.invoke({
+    "person": "Elon Musk"
+})
+
+print(response)
