b9651e28a33d4aab4fedcb26f9c8c766812e95de
Om Navale <omnavale899@gmail.com>
2026-08-10 16:57:34 +0530

    Multiple tools created

--- 08_tools/multiple_tools.py
+++ 08_tools/multiple_tools.py
@@ -1,41 +1,61 @@
+from typing import Annotated
+from typing_extensions import TypedDict
+
 from langchain_core.tools import tool
+from langchain_core.messages import HumanMessage
+
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode
+
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
 
 
 @tool
-def add(a: int, b: int) -> int:
+def add(a: float, b: float) -> float:
     """Add two numbers."""
     return a + b
 
 
 @tool
-def subtract(a: int, b: int) -> int:
-    """Subtract two numbers."""
+def subtract(a: float, b: float) -> float:
+    """Subtract b from a."""
     return a - b
 
 
 @tool
-def multiply(a: int, b: int) -> int:
+def multiply(a: float, b: float) -> float:
     """Multiply two numbers."""
     return a * b
-
-
-@tool
-def divide(a: float, b: float) -> float:
-    """Divide two numbers."""
-
-    if b == 0:
-        return 0
-
-    return a / b
 
 
 tools = [
     add,
     subtract,
-    multiply,
-    divide
+    multiply
 ]
 
 
-for tool in tools:
-    print(tool.name)
+tool_node = ToolNode(tools)
+
+graph = StateGraph(State)
+
+graph.add_node("tools", tool_node)
+
+graph.add_edge(START, "tools")
+graph.add_edge("tools", END)
+
+
+app = graph.compile()
+
+result = app.invoke({
+    "messages": [
+        HumanMessage(
+            content="Calculate 10 + 20"
+        )
+    ]
+})
+
+print(result)
