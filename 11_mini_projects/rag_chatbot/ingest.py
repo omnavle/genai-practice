67501f718814379902a58d1d3f43043fc3fee9d3
Om Navale <omnavale899@gmail.com>
2026-08-11 11:37:29 +0530

    database created

---
+++ 11_mini_projects/rag_chatbot/ingest.py
@@ -0,0 +1,31 @@
+from dotenv import load_dotenv
+
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+
+load_dotenv()
+
+loader = TextLoader("data/knowledge.txt")
+
+documents = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=500,
+    chunk_overlap=50
+)
+
+chunks = text_splitter.split_documents(documents)
+
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+Chroma.from_documents(
+    documents=chunks,
+    embedding=embeddings,
+    persist_directory="./chroma_db"
+)
+
+print("Knowledge base created successfully")
